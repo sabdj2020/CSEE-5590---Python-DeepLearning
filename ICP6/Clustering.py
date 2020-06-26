7e3a75668aa13b4914c4e5ed6b89f2c61ca1573f
sabrinadjeddi <32584787+sabrinadjeddi@users.noreply.github.com>
2020-06-25 22:35:02 -0500

    ICP6

--- ICP6/Clustering.py
+++ ICP6/Clustering.py
@@ -2,10 +2,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
-from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 from sklearn import metrics
 from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
 
 # Read the data using pd.read_csv from panda library
 dataset = pd.read_csv('dataset/CC.csv')
@@ -45,7 +46,7 @@ print(x.shape,y.shape)
 # Use the elbow method to find a good number of clusters with the KMeans algorithm
 wcss = []  #Within Cluster Sum of Squares
 
-#elbow method to know the number of clusters
+#elbow method to know the number of clusters # from sklearn.cluster import KMeans
 for i in range(1,10):
     kmeans = KMeans(n_clusters=i,init='k-means++',max_iter=300,n_init=10,random_state=0)
     kmeans.fit(x)
@@ -68,18 +69,24 @@ km = KMeans(n_clusters=nclusters, random_state=seed)
 km.fit(x)   # predict the cluster for each data point
 y_cluster_kmeans = km.predict(x)
 
-# calculate the silhouette score
+# calculate the silhouette score # from sklearn import metrics
 print("*********************************************************")
 print("silhouette score before scaling**************************************")
 from sklearn import metrics
 score = metrics.silhouette_score(x, y_cluster_kmeans)
 print(score)
 
-# Try feature scaling to see if it will improve the Silhouette score
+# Try feature scaling to see if it will improve the Silhouette score #from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 scaler.fit(x)
 x_scaler = scaler.transform(x)
 
+km = KMeans(n_clusters=3)
+km.fit(x_scaler)
+y_cluster_kmeans = km.predict(x_scaler)
+score = metrics.silhouette_score(x_scaler, y_cluster_kmeans)
+print("silhouette score after scaler")
+print(score)
 
 # Apply PCA on the same dataset.
 
@@ -91,10 +98,14 @@ print("the PCA APPLIED")
 print(fdf)
 print("**********************************************")
 
-# Calculate the silhouette score after
-print("silhouette score after scaler and PCA")
+# apply kmean on the PCA results
+print(" KMEAN on PCA results")
 km = KMeans(n_clusters=3)
 km.fit(x_pca)
 y_cluster_kmeans = km.predict(x_pca)
 score = metrics.silhouette_score(x_pca, y_cluster_kmeans)
+print(" KMEAN + PCA score is")
 print(score)
+
+
+
